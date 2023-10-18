5fbf7f0976b40be143fdee55148472b8260fb9ad
augusstt06 <augusstt06@gmail.com>
2023-10-18 15:21:21 +0900

    Install Redux for Darkmode state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Header from "./component/Header";
+import { Providers } from "./store/provider";
 
 export default function RootLayout({
   children,
@@ -7,11 +8,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </Providers>
   );
 }
