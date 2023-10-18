b5e72bf0ec095295dc0ace5bbeef2bc323fc0fea
augusstt06 <augusstt06@gmail.com>
2023-10-18 15:38:25 +0900

    Darkmode Reducer test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./store";
+
 export default function Home() {
-  return <main></main>;
+  const isDark: boolean = useSelector((state: RootState) => state.value);
+
+  return <main>현재 테마 {isDark ? "Dark Mode" : "Light Mode"}</main>;
 }
