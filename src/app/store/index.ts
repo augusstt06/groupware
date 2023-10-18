a690b9112050c4a446436cf2f8f284e305cb7d4f
augusstt06 <augusstt06@gmail.com>
2023-10-18 15:38:09 +0900

    Create a reducer for Darkmode

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -6,5 +6,5 @@ export const store = configureStore({
   reducer: {},
 });
 
-export type RoosState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
