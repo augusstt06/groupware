e2dd21a59a4b76e349ba6804b6dc285808b57af6
augusstt06 <augusstt06@gmail.com>
2023-10-26 17:08:40 +0900

    Signin button in login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import { SigninBtn } from "./component/Sign/SignBtn";
+import { SignupBtn } from "./component/Sign/SignBtn";
 import Link from "next/link";
 
 export default function Home() {
   return (
+    // 로그인 여부에 따라서 화면 변경
+
+    // 비로그인시
     <main className="flex flex-col justify-center items-center p-10">
       <div className="text-xl font-bold mb-10">
         Easily collaborate with your team from anywhere
@@ -10,9 +13,10 @@ export default function Home() {
       <div className="text-medium font-semibold mb-10">other comments...</div>
       <div>
         <Link href={"/signup"}>
-          <SigninBtn />
+          <SignupBtn />
         </Link>
       </div>
     </main>
+    // 로그인시
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,11 @@
 import { GitSignin, GoogleSignin } from "../component/Sign/SignBtn";
+import { SigninBtn } from "../component/Sign/SignBtn";
 
 export default function Sign() {
   // 추가적으로 넣을것 생각하기
   return (
-    <div className="flex flex-col justify-center items-center p 1-">
-      <div className="my-10 w-3/5">
+    <div className="flex flex-col justify-center items-center p 1">
+      <div className="mt-10 mb-5 w-3/5">
         {/* Email 입력란 */}
         <label
           htmlFor="input-group-1"
@@ -58,7 +59,11 @@ export default function Sign() {
             placeholder="elonmusk"
           />
         </div>
+        <div className="flex flex-col justify-center items-center p 1 mt-5">
+          <SigninBtn />
+        </div>
       </div>
+
       <div className="flex flex-col justify-center items-center font-sm">
         <a className="mb-5">If tou already have account,</a>
         <div className="flex flex-col justify-center items-center font-sm">
