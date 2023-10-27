61110972ed08c5b742a2d045dddd5ab7169dc86f
augusstt06 <augusstt06@gmail.com>
2023-10-27 18:51:29 +0900

    Reflection of sign in request : check id, pwd is duplicate

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,15 @@
-import { GitSignin, GoogleSignin } from "../component/Sign/SignBtn";
-import { SigninBtn } from "../component/Sign/SignBtn";
+"use client";
+
+import { useState } from "react";
+import {
+  GitSignin,
+  GoogleSignin,
+  SigninBtn,
+  DuplicateBtn,
+} from "../component/Sign/SignBtn";
 
 export default function Sign() {
+  const [viewPwd, setViewPwd] = useState(false);
   // 추가적으로 넣을것 생각하기
   return (
     <div className="flex flex-col justify-center items-center p 1">
@@ -11,7 +19,7 @@ export default function Sign() {
           htmlFor="input-group-1"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          Enter Email
+          Email (for your ID)
         </label>
         <div className="relative mb-6">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
@@ -30,17 +38,31 @@ export default function Sign() {
             type="text"
             id="input-group-1"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
+            placeholder="name@sample.com"
           />
+          {/* 아이디 중복체크 => 서버에 api 요청 */}
+          <div className="absolute inset-y-0 right-4 flex items-center pl-3.5">
+            <input type="checkbox" className="cursor-pointer w-5 h-5" />
+          </div>
         </div>
-        {/* 기타 필요사항 입력란 */}
+
+        {/* 비밀번호 입력란 */}
         <label
           htmlFor="website-admin"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
         >
-          Username (and others...)
+          Password (6글자 이상, 숫자, 특수문자,영대문자 포함)
         </label>
-        <div className="flex">
+        <label className="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          입력문자 보기
+        </label>
+        <input
+          type="checkbox"
+          className="ml-2"
+          defaultChecked={viewPwd}
+          onChange={() => setViewPwd(!viewPwd)}
+        />
+        <div className="flex relative mt-2 mb-6">
           <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
             <svg
               className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -53,12 +75,17 @@ export default function Sign() {
             </svg>
           </span>
           <input
-            type="text"
+            type={`${viewPwd ? "text" : "password"}`}
             id="website-admin"
             className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="elonmusk"
           />
+          {/* 비밀번호 중복체크 => 서버에 api 요청 */}
+          <div className="absolute inset-y-0 right-4 flex items-center pl-3.5">
+            <input type="checkbox" className="cursor-pointer w-5 h-5" />
+          </div>
         </div>
+
         <div className="flex flex-col justify-center items-center p 1 mt-5">
           <SigninBtn />
         </div>
