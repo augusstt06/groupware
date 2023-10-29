1af2b04fbbfd33f8a51b481e8415e2832c7b3fb9
augusstt06 <augusstt06@gmail.com>
2023-10-29 12:58:22 +0900

    Separate progress bar items

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,40 @@
 "use client";
 
 import { useState } from "react";
-import Step1 from "../component/pages/login-step/step1";
+import Step1 from "../component/pages/login-step/Step1";
 import Step2 from "../component/pages/login-step/Step2";
 import { SignupBtn } from "../component/ui/button/LoginBtn";
 import Progressbar from "../component/ui/progressbar/Progressbar";
 
 export default function Sign() {
   const [viewPwd, setViewPwd] = useState(false);
-  const [isItemFull, setIsItemFUll] = useState({
+  const [isStepComplte, setIsStepComplete] = useState({
     step1: false,
     step2: false,
   });
+
+  // 추후에 nextStep, backStep 함수 구조 바꾸기
+  const nextStep = () => {
+    setIsStepComplete({
+      step1: true,
+      step2: false,
+    });
+  };
+
+  const backStep = () => {
+    setIsStepComplete({
+      step1: false,
+      step2: false,
+    });
+  };
+
+  //  step1을 ture로 만드는 조건 : 중복체크가 완료되었다는 응답을 받았을때
   const [items, setItems] = useState({
     allItems: 5,
     completedItems: 0,
   });
-  const entryCompleted = () => {
+
+  const itemCompleted = () => {
     if (items.completedItems === items.allItems) return;
     setItems({
       allItems: 5,
@@ -26,15 +44,30 @@ export default function Sign() {
 
   return (
     <div className="flex flex-col justify-center items-center p 1">
-      <div className="mt-10 mb-5 w-3/5">
-        {!isItemFull.step1 ? (
+      <div className="mt-10 mb-3 w-3/5">
+        {!isStepComplte.step1 ? (
           <Step1 viewPwd={viewPwd} setViewPwd={setViewPwd} />
         ) : (
           <Step2 />
         )}
 
-        <div className="flex flex-col justify-center items-center p 1 mt-5">
-          <SignupBtn title={!isItemFull.step1 ? "Next" : "Sign In"} />
+        <div className="flex flex-row justify-center items-center mb-5">
+          {isStepComplte.step1 ? (
+            <div
+              className="flex flex-col justify-center items-center p 1 mt-5"
+              onClick={backStep}
+            >
+              <SignupBtn title="Back" />
+            </div>
+          ) : (
+            <></>
+          )}
+          <div
+            className="flex flex-col justify-center items-center p 1 mt-5"
+            onClick={nextStep}
+          >
+            <SignupBtn title={!isStepComplte.step1 ? "Next" : "Sign In"} />
+          </div>
         </div>
         <div>
           <Progressbar
