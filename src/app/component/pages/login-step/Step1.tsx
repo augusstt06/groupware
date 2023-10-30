00af4fde958962f0267ae65985c0bd416061b57c
augusstt06 <augusstt06@gmail.com>
2023-10-30 13:09:37 +0900

    Fix variable name

--- src/app/component/pages/login-step/Step1.tsx
+++ src/app/component/pages/login-step/Step1.tsx
@@ -3,8 +3,8 @@ import { SigninHideInput, SigninInput } from "../../ui/input/SignInput";
 import { RiLockPasswordFill } from "react-icons/ri";
 
 type Props = {
-  viewPwd: boolean;
-  setViewPwd: (view: boolean) => void;
+  isView: boolean;
+  setIsView: (view: boolean) => void;
 };
 export default function Step1(props: Props) {
   return (
@@ -19,8 +19,8 @@ export default function Step1(props: Props) {
         title="Password (6글자 이상, 숫자, 특수문자,영대문자 포함)"
         placeholder="Asd12!!"
         icon={<RiLockPasswordFill />}
-        view={props.viewPwd}
-        setView={props.setViewPwd}
+        view={props.isView}
+        setView={props.setIsView}
         checkBox_dup={true}
       />
     </>
