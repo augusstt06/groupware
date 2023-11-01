80c5a399b766690d56af7c5ed1c5c0cee3f64f97
augusstt06 <augusstt06@gmail.com>
2023-11-01 14:21:10 +0900

    Create module dir : Divide custom hook & util function

--- src/app/module/hooks/reactHooks/index.tsx
+++ src/app/module/hooks/reactHooks/index.tsx
@@ -1,11 +1,4 @@
 import React, { useState } from "react";
-import { useAppDispatch } from "../reduxHooks";
-import {
-  emailCheckReducer,
-  pwdCheckReducer,
-} from "@/app/store/reducers/validReducer";
-import { useFetchProps } from "@/app/types";
-import axios from "axios";
 
 export const useInput = (state: string) => {
   const [value, setValue] = useState("");
