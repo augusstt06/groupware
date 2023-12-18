6ef24dbcd276e9d29d6b484b36cf25f4892ead07
augusstt06 <augusstt06@gmail.com>
2023-12-18 19:40:38 +0900

    fix : post modal

--- src/app/component/ui/modal/BoardWriteModal.tsx
+++ src/app/component/ui/modal/BoardWriteModal.tsx
@@ -33,8 +33,8 @@ export default function BoardWriteModal(props: BoardWriteModalprops) {
               <div className="w-2/3 text-center">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">게시글 설정</h3>
               </div>
-              <div className="w-1/5 flex flex-row items-center justify-around">
-                <button className="mt-3 mb-3 w-2/5  md:text-sm text-xs text-indigo-500 hover:text-white dark:text-white dark:bg-indigo-500 dark:border-white border-indigo-500 hover:bg-indigo-500 rounded-lg text-center items-center dark:hover:bg-white dark:hover:text-indigo-500 border-2 dark:hover:border-indigo-500/75">
+              <div className="w-2/5 flex flex-row items-center justify-around">
+                <button className="mt-3 mb-3 w-2/5 md:text-sm text-xs text-indigo-500 hover:text-white dark:text-white dark:bg-indigo-500 dark:border-white border-indigo-500 hover:bg-indigo-500 rounded-lg text-center items-center dark:hover:bg-white dark:hover:text-indigo-500 border-2 dark:hover:border-indigo-500/75">
                   임시저장
                 </button>
                 <button className="mt-3 mb-3 w-2/5 md:text-sm text-xs text-indigo-500 hover:text-white dark:text-white dark:bg-indigo-500 dark:border-white border-indigo-500 hover:bg-indigo-500 rounded-lg text-center items-center dark:hover:bg-white dark:hover:text-indigo-500 border-2 dark:hover:border-indigo-500/75">
