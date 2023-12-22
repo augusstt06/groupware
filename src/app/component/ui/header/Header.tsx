16cb036db08f6c3d5f130cf7c00901cfdaac2974
augusstt06 <augusstt06@gmail.com>
2023-12-22 19:57:27 +0900

    fix : rename branch

--- src/app/component/ui/header/Header.tsx
+++ src/app/component/ui/header/Header.tsx
@@ -39,6 +39,7 @@ export default function Header() {
     board: false,
     project: false,
   })
+  //
   const accessToken = moduleGetCookie(KEY_ACCESS_TOKEN)
   const loginCompleteState = useAppSelector((state) => state.maintain[KEY_LOGIN_COMPLETE])
   const handleOpen = (title: string) => {
