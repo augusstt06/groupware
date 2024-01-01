38614c17860f4ec640b779113d3c107bec9864e7
augusstt06 <augusstt06@gmail.com>
2024-01-01 14:34:52 +0900

    add : content view component

--- src/app/board/detail/page.tsx
+++ src/app/board/detail/page.tsx
@@ -1,8 +1,9 @@
+'use client'
 // 상세페이지
 import { useEffect, useState } from 'react'
 
-import { Viewer } from '@toast-ui/react-editor'
 import '@toast-ui/editor/dist/toastui-editor-viewer.css'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 
 import Sidebar from '@/app/component/ui/sidebar/Sidebar'
@@ -12,6 +13,9 @@ import { moduleCheckUserState } from '@/app/module/utils/moduleCheckUserState'
 import { moduleGetCookie } from '@/app/module/utils/moduleCookie'
 import { type ModuleCheckUserStateProps } from '@/app/types/moduleTypes'
 
+const Viewbox = dynamic(async () => import('../../component/ui/editor/TextViewer'), {
+  ssr: false,
+})
 export default function BoardDetail() {
   // 추후 content reponse 받은것을 바탕으로 Viewer의 props로 넘기기
   const router = useRouter()
@@ -31,7 +35,7 @@ export default function BoardDetail() {
     <main className="w-full grid gap-4 grid-cols-4 h-4/5 pt-10 md:ml-10 md:mr-10 ml-5 z-1">
       <Sidebar title={BOARD} />
       <div className="md:col-span-3 mr-10 col-span-4">
-        <Viewer />
+        <Viewbox />
       </div>
     </main>
   )
---
+++ src/app/component/ui/editor/TextViewer.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { Viewer } from '@toast-ui/react-editor'
+
+export default function TextViewer() {
+  return <Viewer />
+}
