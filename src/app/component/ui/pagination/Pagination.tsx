f4fd21952ae2705da9150d338dcb93525e857480
augusstt06 <augusstt06@gmail.com>
2024-01-04 15:26:33 +0900

    fix : ui

--- src/app/component/ui/pagination/Pagination.tsx
+++ src/app/component/ui/pagination/Pagination.tsx
@@ -1,4 +1,5 @@
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight } from 'react-icons/md'
 
 import { type PaginationProps } from '@/app/types/pageTypes'
 
@@ -7,6 +8,12 @@ export default function Pagination(props: PaginationProps) {
     if (props.pageNumber === props.size - 1) return
 
     props.setPageNumber(props.pageNumber + 1)
+  }
+  const moveStartBoardList = () => {
+    props.setPageNumber(0)
+  }
+  const moveEndBoardList = () => {
+    props.setPageNumber(props.size - 1)
   }
   const movePreviousBoardList = () => {
     if (props.pageNumber < 1) return
@@ -25,8 +32,10 @@ export default function Pagination(props: PaginationProps) {
       className="cursor-pointer"
     >
       <a
-        className={`flex items-center justify-center px-4 h-10 leading-tight text-gray-500 ${
-          props.pageNumber === index ? 'bg-gray-200 dark:bg-gray-500' : 'bg-white dark:bg-gray-800'
+        className={`flex items-center justify-center px-4 h-10 leading-tight  ${
+          props.pageNumber === index
+            ? 'bg-gray-200 dark:bg-gray-500 font-bold text-gray-700'
+            : 'bg-white dark:bg-gray-800 text-gray-500'
         } border border-gray-300 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400  dark:hover:text-white`}
       >
         {index + 1}
@@ -36,8 +45,15 @@ export default function Pagination(props: PaginationProps) {
   return (
     <nav aria-label="Page navigation example" className="mt-2 mb-5">
       <ul className="flex items-center -space-x-px h-10 text-base">
-        <li onClick={movePreviousBoardList} className="cursor-pointer">
+        <li onClick={moveStartBoardList} className="cursor-pointer">
           <a className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            <span className="sr-only">Start</span>
+
+            <MdKeyboardDoubleArrowLeft />
+          </a>
+        </li>
+        <li onClick={movePreviousBoardList} className="cursor-pointer">
+          <a className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
             <span className="sr-only">Previous</span>
 
             <IoIosArrowBack />
@@ -47,9 +63,15 @@ export default function Pagination(props: PaginationProps) {
         {loopLiTag}
 
         <li onClick={moveNextBoardList} className="cursor-pointer">
-          <a className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+          <a className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
             <span className="sr-only">Next</span>
             <IoIosArrowForward />
+          </a>
+        </li>
+        <li onClick={moveEndBoardList} className="cursor-pointer">
+          <a className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            <span className="sr-only">End</span>
+            <MdKeyboardDoubleArrowRight />
           </a>
         </li>
       </ul>
