4a07000ac7f88d706c171960eed77997aa663542
augusstt06 <augusstt06@gmail.com>
2024-02-06 15:33:56 +0900

    react-query : board detail

--- src/app/board/detail/[id]/page.tsx
+++ src/app/board/detail/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 
 import '@toast-ui/editor/dist/toastui-editor-viewer.css'
+import { useQuery } from '@tanstack/react-query'
 import dynamic from 'next/dynamic'
 import { useParams, useRouter } from 'next/navigation'
 import { FaHeart, FaRegHeart } from 'react-icons/fa'
@@ -29,13 +30,19 @@ import {
 } from '@/app/constant/route/route-constant'
 import { useAppDispatch, useAppSelector } from '@/app/module/hooks/reduxHooks'
 import { moduleCheckUserState } from '@/app/module/utils/check/moduleCheckUserState'
-import { moduleGetCookie } from '@/app/module/utils/moduleCookie'
+import {
+  checkTokenExpired,
+  moduleDecodeToken,
+  moduleGetCookie,
+  moduleRefreshToken,
+} from '@/app/module/utils/moduleCookie'
 import { moduleDeleteFetch, moduleGetFetch, modulePostFetch } from '@/app/module/utils/moduleFetch'
 import {
   addPostingLikeReducer,
   deletePostingLikeReducer,
 } from '@/app/store/reducers/board/boardLikeReducer'
 import {
+  type CustomDecodeTokenType,
   type FailResponseType,
   type ModuleGetFetchProps,
   type ModulePostFetchProps,
@@ -52,21 +59,21 @@ export default function BoardDetail() {
 
   const dispatch = useAppDispatch()
   const likeState = useAppSelector((state) => state.boardLike.postingLikeList)
+
   const userInfo = useAppSelector((state) => state.userInfo.extraInfo)
-  const [content, setContent] = useState<DetailResponseType>()
   const [commentCount, setCommentCount] = useState<number>(0)
   const [accessToken, setAccessToken] = useState(moduleGetCookie(KEY_ACCESS_TOKEN))
+  const decodeToken = moduleDecodeToken(accessToken)
+  const accessTokenTime = Number((decodeToken as CustomDecodeTokenType).exp)
   const orgCode = useAppSelector((state) => state.userInfo[KEY_X_ORGANIZATION_CODE])
   const loginCompleteState = useAppSelector((state) => state.maintain[KEY_LOGIN_COMPLETE])
   const [isRerender, setIsRerender] = useState<boolean>(false)
-  const [postId, setPostId] = useState<string>('')
   const isModalOpen = useAppSelector((state) => state.openBoardWriteModal.isOpen)
   const [errorState, setErrorState] = useState({
     isError: false,
     description: '',
   })
-  const isPostLike = likeState?.includes(content?.id as number)
-  const isAuthor = content?.name === userInfo.name
+
   const doRerender = () => {
     setIsRerender(!isRerender)
   }
@@ -85,60 +92,68 @@ export default function BoardDetail() {
     return sum
   }
 
-  const fetchGetPostingDetail = async (data: ModuleGetFetchProps) => {
-    try {
-      const res = await moduleGetFetch<DetailResponseType>(data)
-      if (res.status !== API_SUCCESS_CODE) throw new Error((res as FailResponseType).message)
-
-      const contentRes = (res as SuccessResponseType<DetailResponseType>).result
-      setContent(contentRes)
-      if (contentRes.comments !== undefined) {
-        const commentsNum = countComments(contentRes.comments)
-        setCommentCount(commentsNum)
+  const {
+    error: postingError,
+    data: postingData,
+    refetch,
+  } = useQuery<SuccessResponseType<DetailResponseType>>({
+    queryKey: ['posting-detail'],
+    queryFn: async () => {
+      const fetchProps: ModuleGetFetchProps = {
+        params: {
+          postingId: param.id.toString(),
+        },
+        fetchUrl: API_URL_POSTINGS,
+        header: {
+          Authorization: `Bearer ${accessToken}`,
+          [KEY_X_ORGANIZATION_CODE]: orgCode,
+        },
       }
-    } catch (err) {
-      setErrorState({ isError: true, description: errDefault('게시글') })
-    }
-  }
+      const res = await moduleGetFetch<DetailResponseType>(fetchProps)
+      return res as SuccessResponseType<DetailResponseType>
+    },
+  })
+  const isPostLike = likeState?.includes(postingData?.result?.id as number)
+  const isAuthor = postingData?.result?.name === userInfo.name
   const moveBoardListPage = () => {
     router.push(ROUTE_BOARD)
   }
 
-  const fetchPostingLikeProps: ModulePostFetchProps = {
-    data: {
-      postingId: content?.id,
-    },
-    fetchUrl: API_URL_POSTINGS_LIKE,
-    header: {
-      Authorization: `Bearer ${accessToken}`,
-      [KEY_X_ORGANIZATION_CODE]: orgCode,
-    },
-  }
   const fetchPostingLike = async () => {
     try {
-      await modulePostFetch<string>(fetchPostingLikeProps)
-      dispatch(addPostingLikeReducer(content?.id as number))
-      doRerender()
+      const fetchProps: ModulePostFetchProps = {
+        data: {
+          postingId: postingData?.result.id,
+        },
+        fetchUrl: API_URL_POSTINGS_LIKE,
+        header: {
+          Authorization: `Bearer ${accessToken}`,
+          [KEY_X_ORGANIZATION_CODE]: orgCode,
+        },
+      }
+      await modulePostFetch<string>(fetchProps)
+      await refetch()
+      dispatch(addPostingLikeReducer(postingData?.result.id as number))
     } catch (err) {
       alert(errDefault('좋아요'))
     }
   }
 
-  const fetchPostingUnLikeProps: ModulePostFetchProps = {
-    data: {
-      postingId: content?.id,
-    },
-    fetchUrl: API_URL_POSTINGS_UNLIKE,
-    header: {
-      Authorization: `Bearer ${accessToken}`,
-      [KEY_X_ORGANIZATION_CODE]: orgCode,
-    },
-  }
   const fetchPostingUnLike = async () => {
     try {
-      await modulePostFetch<string>(fetchPostingUnLikeProps)
-      dispatch(deletePostingLikeReducer(content?.id as number))
-      doRerender()
+      const fetchProps: ModulePostFetchProps = {
+        data: {
+          postingId: postingData?.result.id,
+        },
+        fetchUrl: API_URL_POSTINGS_UNLIKE,
+        header: {
+          Authorization: `Bearer ${accessToken}`,
+          [KEY_X_ORGANIZATION_CODE]: orgCode,
+        },
+      }
+      await modulePostFetch<string>(fetchProps)
+      await refetch()
+      dispatch(deletePostingLikeReducer(postingData?.result.id as number))
     } catch (err) {
       alert(errDefault('좋아요 취소'))
     }
@@ -149,19 +164,19 @@ export default function BoardDetail() {
     else void fetchPostingLike()
   }
 
-  const fetchDeletePostingsProps: ModuleGetFetchProps = {
-    params: {
-      postingId: content?.id as number,
-    },
-    fetchUrl: API_URL_POSTINGS,
-    header: {
-      Authorization: `Bearer ${accessToken}`,
-      [KEY_X_ORGANIZATION_CODE]: orgCode,
-    },
-  }
   const fetchDeletePostings = async () => {
     try {
-      const res = await moduleDeleteFetch<string>(fetchDeletePostingsProps)
+      const fetchProps: ModuleGetFetchProps = {
+        params: {
+          postingId: postingData?.result.id as number,
+        },
+        fetchUrl: API_URL_POSTINGS,
+        header: {
+          Authorization: `Bearer ${accessToken}`,
+          [KEY_X_ORGANIZATION_CODE]: orgCode,
+        },
+      }
+      const res = await moduleDeleteFetch<string>(fetchProps)
       if (res.status !== API_SUCCESS_CODE) throw new Error((res as FailResponseType).message)
       router.back()
     } catch (err) {
@@ -172,62 +187,72 @@ export default function BoardDetail() {
     void fetchDeletePostings()
   }
 
+  if (postingError !== null) {
+    setErrorState({ isError: true, description: errDefault('게시글') })
+  }
+
   useEffect(() => {
-    setPostId(param.id.toString())
-    const fetchGetPostingDetailProps: ModuleGetFetchProps = {
-      params: {
-        postingId: postId,
-      },
-      fetchUrl: API_URL_POSTINGS,
-      header: {
-        Authorization: `Bearer ${accessToken}`,
-        [KEY_X_ORGANIZATION_CODE]: orgCode,
-      },
+    if (checkTokenExpired(accessTokenTime)) {
+      void moduleRefreshToken(accessToken)
     }
+    moduleCheckUserState({ loginCompleteState, router, accessToken, setAccessToken })
+  }, [accessToken])
 
-    if (postId !== '') {
-      void fetchGetPostingDetail(fetchGetPostingDetailProps)
-    }
-
+  useEffect(() => {
     if (errorState.isError) {
       router.push(ROUTE_ERR_NOT_FOUND_POSTING_DETAIL)
     }
+  }, [isRerender, postingData?.result.id, accessToken])
 
-    moduleCheckUserState({ loginCompleteState, router, accessToken, setAccessToken })
-  }, [isRerender, postId, accessToken])
+  useEffect(() => {
+    if (postingData?.result !== undefined) {
+      if (postingData.result.comments !== undefined) {
+        const commentsNum = countComments(postingData.result.comments)
+        setCommentCount(commentsNum)
+      }
+    }
+  }, [postingData, doRerender])
 
   return (
     <>
-      {content != null ? (
+      {postingData?.result !== undefined ? (
         <main className="w-full 2xl:w-2/3 h-4/5 flex flex-col items-center">
           <div className="w-4/5 rounded md:col-span-3 mr-10 col-span-4 bg-white dark:bg-gray-700 dark:text-white p-5 border-2">
             <PostingDetailHeader
               moveBoardListPage={moveBoardListPage}
               commentCount={commentCount}
-              content={content}
+              content={postingData?.result}
               isAuthor={isAuthor}
               clickDelete={clickDelete}
             />
             <div className="border-b-2 border-gray-300 pt-2 pb-4">
-              <Viewbox content={content.content} />
+              <Viewbox content={postingData.result.content} />
               <div
                 className="cursor-pointer border-2 rounded-lg border-red-400 p-2 flex flex-row justify-around items-center w-16 hover:font-bold hover:bg-red-400 hover:text-white text-red-400"
                 onClick={clickLike}
               >
                 {isPostLike ? <FaHeart className="w-3 h-3" /> : <FaRegHeart className=" w-3 h-3" />}
-                <span className="text-xs">{content.like}</span>
+                <span className="text-xs">{postingData.result.like}</span>
               </div>
             </div>
             <div className="pt-2 pb-2 ">
               <span className="font-bold text-base">댓글 {commentCount}</span>
-              {content.comments?.map((data) => (
+              {postingData?.result.comments?.map((data) => (
                 <div key={data.content} className="border-b-1 border-gray-300">
-                  <Comment comments={data} postingID={content.id} doRerender={doRerender} />
+                  <Comment
+                    comments={data}
+                    postingID={postingData?.result.id}
+                    doRerender={doRerender}
+                  />
                 </div>
               ))}
             </div>
             <div>
-              <WriteComment postingID={content.id} parentID={null} doRerender={doRerender} />
+              <WriteComment
+                postingID={postingData?.result.id}
+                parentID={null}
+                doRerender={doRerender}
+              />
             </div>
             {isModalOpen ? <BoardWriteModal currentBoard={null} /> : <></>}
           </div>
