48e106ca9fc028a9727a831eb101ab4d22764609
augusstt06 <augusstt06@gmail.com>
2024-03-03 17:23:24 +0900

    fix : project todo ui, main page ui

--- src/app/component/page/main/hub/MainHub.tsx
+++ src/app/component/page/main/hub/MainHub.tsx
@@ -3,18 +3,26 @@ import MainTab from '@/app/component/ui/tab/main/MainTab'
 import { type MainHubProps } from '@/app/types/ui/extra'
 
 export default function MainHub(props: MainHubProps) {
-  // FIXME: response되는 게시글을  아래 TodoCard 이용하여 mapping
   const { currentPostings } = props
+  const isCurrentPostingExist = () => {
+    return currentPostings.length !== 0
+  }
   return (
     <div className="w-4/5 max-w-7xl flex flex-col items-center space-y-5">
       <MainTab />
 
-      <section className="w-full flex flex-col items-center rounded-xl shadow-lg p-2 truncate bg-[#f5f7fc] bg-opacity-70 dark:bg-opacity-10 space-y-3">
-        <h1 className="p-3">최근 게시물</h1>
-        {currentPostings.map((data, index) => (
-          <BoardCard key={index} content={data} />
-        ))}
-      </section>
+      {isCurrentPostingExist() ? (
+        <section className="w-full flex flex-col items-center rounded-xl shadow-lg p-2 truncate bg-[#f5f7fc] bg-opacity-70 dark:bg-opacity-10 space-y-3">
+          <h1 className="p-3">최근 게시물</h1>
+          {currentPostings.map((data, index) => (
+            <BoardCard key={index} content={data} />
+          ))}
+        </section>
+      ) : (
+        <section className="w-full flex flex-col items-center rounded-xl shadow-lg p-2 truncate bg-[#f5f7fc] bg-opacity-70 dark:bg-opacity-10 space-y-3">
+          <h1>최근 작성된 게시물이 없습니다.</h1>
+        </section>
+      )}
     </div>
   )
 }
