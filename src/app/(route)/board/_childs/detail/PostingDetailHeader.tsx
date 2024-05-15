31d2a23a3599d462bd820a9720c9db6fbf5966ad
augusstt06 <augusstt06@gmail.com>
2024-05-15 19:51:38 +0900

    fix: board detail

--- src/app/(route)/board/_childs/detail/PostingDetailHeader.tsx
+++ src/app/(route)/board/_childs/detail/PostingDetailHeader.tsx
@@ -14,25 +14,43 @@ type PostingDetailHeaderProps = {
 }
 
 export default function PostingDetailHeader(props: PostingDetailHeaderProps) {
+  const { moveBoardListPage, commentCount, content, isAuthor, clickDelete } = props
   return (
     <div>
-      <div
-        className="flex flex-row mb-3 items-center cursor-pointer inline w-20 hover:font-bold"
-        onClick={props.moveBoardListPage}
-      >
-        <IoIosArrowBack className="mr-2" />
-        <span className="text-xs">목록보기</span>
+      <div className="sort-row-flex justify-between mb-3">
+        <section
+          className="smooth-transition  sort-row-flex cursor-pointer w-24 hover:bg-gray-300 border-2 border-gray-400 rounded-lg p-2 space-x-2"
+          onClick={moveBoardListPage}
+        >
+          <IoIosArrowBack />
+          <span className="text-xs">목록보기</span>
+        </section>
+        {isAuthor ? (
+          <div className="sort-row-flex justify-between w-40">
+            <button className="smooth-transition  w-16 md:text-sm text-xs bg-indigo-400 text-white dark:border-white border-indigo-400 hover:bg-indigo-600 rounded-lg text-center items-center border-2 p-2">
+              수정
+            </button>
+            <button
+              className="smooth-transition w-16 md:text-sm text-xs bg-red-400 text-white  dark:border-white border-red-400 hover:bg-red-600 rounded-lg text-center items-center border-2 p-2"
+              onClick={clickDelete}
+            >
+              삭제
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
 
-      <div className="flex flex-col border-b-2 border-gray-300 pb-2 truncate">
-        <span className="text-xl font-bold mb-2">{props.content.title}</span>
-        <span className="text-xs">
-          {props.content.name}({props.content.position})
+      <div className="flex flex-col border-b-2 border-gray-300 truncate pt-5 pb-5 space-y-3">
+        <span className="text-xl font-bold">{content.title}</span>
+        <span className="text-sm">
+          {content.name}({content.position})
         </span>
         <div className="flex flex-row justify-between">
           <div className="flex flex-row w-2/5 justify-start items-center text-gray-400">
             <span className="text-xs justify-around items-center mr-3">
-              {moduleConvertDate(props.content?.createdAt, '.', false).split(' ')[0]}
+              {moduleConvertDate(content?.createdAt, '.', false).split(' ')[0]}
             </span>
             {/* 조회수 */}
             <div className="flex flex-row text-xs justify-around items-center w-6 mr-2">
@@ -42,30 +60,14 @@ export default function PostingDetailHeader(props: PostingDetailHeaderProps) {
             {/* 좋아요 */}
             <div className="flex flex-row text-xs justify-around items-center w-6 mr-2">
               <FaHeart className="text-red-400" />
-              <span>{props.content.like}</span>
+              <span>{content.like}</span>
             </div>
             {/* 댓글 수 */}
             <div className="flex flex-row text-xs justify-around items-center w-6">
               <FaRegComment />
-              <span>{props.commentCount}</span>
+              <span>{commentCount}</span>
             </div>
           </div>
-          {/* FIXME: 본인 글에만 표시 */}
-          {props.isAuthor ? (
-            <div className="flex flex-row items-center justify-around w-1/3">
-              <button className="w-2/5 md:text-sm text-xs text-indigo-500 hover:text-white dark:text-white dark:bg-indigo-500 dark:border-white border-indigo-500 hover:bg-indigo-500 rounded-lg text-center items-center dark:hover:bg-white dark:hover:text-indigo-500 border-2 dark:hover:border-indigo-500/75">
-                수정
-              </button>
-              <button
-                className="w-2/5 md:text-sm text-xs text-indigo-500 hover:text-white dark:text-white dark:bg-indigo-500 dark:border-white border-indigo-500 hover:bg-indigo-500 rounded-lg text-center items-center dark:hover:bg-white dark:hover:text-indigo-500 border-2 dark:hover:border-indigo-500/75"
-                onClick={props.clickDelete}
-              >
-                삭제
-              </button>
-            </div>
-          ) : (
-            <></>
-          )}
         </div>
       </div>
     </div>
--- src/app/(route)/board/detail/[id]/page.tsx
+++ src/app/(route)/board/detail/[id]/page.tsx
@@ -217,7 +217,7 @@ export default function BoardDetail() {
   return (
     <>
       {postingData?.result !== undefined ? (
-        <section className="w-full h-4/5 flex flex-col items-center ">
+        <section className="w-full h-4/5 flex flex-col items-center pr-5 pl-5 ml-5 ">
           <div className="w-4/5 md:col-span-3 mr-10 col-span-4 dark:text-white p-5 border-2 rounded-xl shadow mb-5 bg-[#f5f7fc]  bg-opacity-70 dark:bg-opacity-10">
             <PostingDetailHeader
               moveBoardListPage={moveBoardListPage}
