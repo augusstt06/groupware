5acede47019d756b40195a290e129175f1672f19
augusstt06 <augusstt06@gmail.com>
2024-05-20 14:12:02 +0900

    fix: layouts

--- src/app/(route)/main/layout.tsx
+++ src/app/(route)/main/layout.tsx
@@ -1,10 +1,7 @@
 'use client'
-
 import { useEffect, useState } from 'react'
 
-import { useRouter, useSearchParams } from 'next/navigation'
-
-import BoardWriteModal from './_childs/modal/BoardWriteModal'
+import { useRouter } from 'next/navigation'
 
 import SettingModal from '@/_component/modal/setting/SettingModal'
 import { KEY_ACCESS_TOKEN, KEY_LOGIN_COMPLETE } from '@/constant/constant'
@@ -20,24 +17,18 @@ import { handleSettingModalReducer } from '@/store/reducers/setting/settingModal
 import { type CustomDecodeTokenType } from '@/types/module'
 import { type ReactProps } from '@/types/pageType'
 
-export default function BoardLayout({ children }: ReactProps) {
+export default function MainLayout({ children }: ReactProps) {
   const router = useRouter()
   const dispatch = useAppDispatch()
-  const query = useSearchParams().get('name')
   const isSettingOpen = useAppSelector((state) => state.settingModal.isOpen)
   const [accessToken, setAccessToken] = useState(moduleGetCookie(KEY_ACCESS_TOKEN))
   const decodeToken = moduleDecodeToken(accessToken)
 
   const loginCompleteState = useAppSelector((state) => state.maintain[KEY_LOGIN_COMPLETE])
-
-  const currentBoard = useAppSelector((state) => state.boardCategory.myBoard).filter(
-    (data) => data.name === query,
-  )[0]
-
-  const isModalOpen = useAppSelector((state) => state.openBoardWriteModal.isOpen)
   useEffect(() => {
     if (isSettingOpen) dispatch(handleSettingModalReducer())
   }, [])
+
   useEffect(() => {
     const accessTokenTime = Number((decodeToken as CustomDecodeTokenType).exp)
     const isTokenExist: boolean = checkTokenExpired(accessTokenTime)
@@ -46,10 +37,11 @@ export default function BoardLayout({ children }: ReactProps) {
     }
     moduleCheckUserState({ loginCompleteState, router, accessToken, setAccessToken })
   }, [accessToken])
+
   return (
     <>
       {children}
-      {isModalOpen ? <BoardWriteModal currentBoard={currentBoard} /> : <></>}
+
       {isSettingOpen ? <SettingModal /> : null}
     </>
   )
